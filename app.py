0f2c30ad7ddbf6eae0a08bf4b7f36dbb0ce3299f
ErmiasKetos <123589824+ErmiasKetos@users.noreply.github.com>
2024-11-17 06:45:21 -0800

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from scr.advanced_analysis import voltage_profiles, polarization_analysis, dq_de
 from scr.visualization import plot_capacity_vs_cycle, plot_voltage_vs_capacity, plot_dq_de_curves
 from scr.li_s_features import polysulfide_shuttle_assessment, lithium_metal_anode_monitoring
 from scr.ml_analysis import predict_capacity, detect_anomalies, estimate_rul
+from scr.ml_analysis import predict_capacity, detect_anomalies, estimate_rul
 
 
 st.set_page_config(page_title="Advanced Battery Data Analyzer", layout="wide")
@@ -84,20 +85,20 @@ if uploaded_files:
         with col2:
             st.subheader("Lithium Metal Anode Monitoring")
             lithium_metal_anode_monitoring(df)
-    
+
     elif analysis_type == "ML Analysis":
-        st.header("Machine Learning Analysis")
-        
-        tab1, tab2, tab3 = st.tabs(["Capacity Prediction", "Anomaly Detection", "RUL Estimation"])
-        
-        with tab1:
-            predict_capacity(df)
-        
-        with tab2:
-            detect_anomalies(df)
-        
-        with tab3:
-            estimate_rul(df)
+    st.header("Machine Learning Analysis")
+    
+    tab1, tab2, tab3 = st.tabs(["Capacity Prediction", "Anomaly Detection", "RUL Estimation"])
+    
+    with tab1:
+        predict_capacity(df)
+    
+    with tab2:
+        detect_anomalies(df)
+    
+    with tab3:
+        estimate_rul(df)
     
     # Data Export
     st.sidebar.header("Data Export")
